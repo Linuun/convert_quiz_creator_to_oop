f6784682ab36f65d377710f83a637e57cae74f37
Linuun <jlenonninolla31@gmail.com>
2025-05-20 22:29:59 +0800

    set a class that shows attributes like color and file name, and use a constructor that sets the file name

--- QuizBase.py
+++ QuizBase.py
@@ -1,2 +1,11 @@
 # class that defines shared attributes like color codes and file name
 # constructor that sets the file name 
+class QuizBase:
+    RED = '\033[91m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    CYAN = '\033[96m'
+    RESET = '\033[0m'
+
+    def __init__(self, file_name="quiz.txt"):
+        self.file_name = file_name
