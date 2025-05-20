e6a623d4d3fb4de669b2779a9cae61204f4e5442
Linuun <jlenonninolla31@gmail.com>
2025-05-20 22:43:31 +0800

    added pseudocode

---
+++ QuestionInput.py
@@ -0,0 +1,4 @@
+# class responsible for taking user input for questions
+# ask user to input a question, the possible answers, and the correct answer
+# validate the correct answer
+# return all question data as a dictionary
