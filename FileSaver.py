b0bcb577d3f2501d9e68e693d496d0fcb3e61090
Linuun <jlenonninolla31@gmail.com>
2025-05-20 23:13:34 +0800

    set a class responsible for saving the quiz questions to a file

--- FileSaver.py
+++ FileSaver.py
@@ -1,2 +1,5 @@
 # set a class responsible for saving the quiz questions to a file
+from QuizBase import QuizBase
+class FileSaver(QuizBase):
+    def save_to_file(self, questions_data):
 # open the file in append mode and write the questions and answers
