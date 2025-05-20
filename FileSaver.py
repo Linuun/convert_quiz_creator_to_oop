054b20e0d18cb2ea7fa4de3cd20a7a99378aed7a
Linuun <jlenonninolla31@gmail.com>
2025-05-20 23:19:43 +0800

    open the file in append mode and write the questions and answers

--- FileSaver.py
+++ FileSaver.py
@@ -3,3 +3,12 @@ from QuizBase import QuizBase
 class FileSaver(QuizBase):
     def save_to_file(self, questions_data):
 # open the file in append mode and write the questions and answers
+        with open(self.filename, "a") as file:
+            file.write(f"Question: {questions_data['question']}\n")
+            file.write(f"A.) {questions_data['A']}\n")
+            file.write(f"B.) {questions_data['B']}\n")
+            file.write(f"C.) {questions_data['C']}\n")
+            file.write(f"D.) {questions_data['D']}\n")
+            file.write(f"The correct answer is: {questions_data['correct']}\n")
+            file.write(f"=======================\n")
+
