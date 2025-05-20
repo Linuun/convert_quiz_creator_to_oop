de7db7144a3a41af265c123e3dd6db40944ec45a
Linuun <jlenonninolla31@gmail.com>
2025-05-20 23:30:42 +0800

    set a class that combines both question input and file saving logic

--- QuizBuilder.py
+++ QuizBuilder.py
@@ -1,4 +1,8 @@
 # set a class the combines both question input and file saving logic
+from QuestionInput import QuestionInput
+from FileSaver import FileSaver
+class QuizBuilder(QuestionInput, FileSaver):
+    def run(self):
 # use while loop
 # ask the user to input a question
 # save the question if input is valid
