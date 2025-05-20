b46824173047524db95d418ae20acdf9604ca793
Linuun <jlenonninolla31@gmail.com>
2025-05-20 23:27:49 +0800

    added pseudocode

---
+++ QuizBuilder.py
@@ -0,0 +1,6 @@
+# set a class the combines both question input and file saving logic
+# use while loop
+# ask the user to input a question
+# save the question if input is valid
+# ask the user if they want to add another question
+# break the loop if they say no
