c6171ca9dbfe2f904f013ffc65420bc0ff57b2da
Linuun <jlenonninolla31@gmail.com>
2025-05-20 22:49:35 +0800

    set a class responsible for getting questions from the user

--- QuestionInput.py
+++ QuestionInput.py
@@ -1,4 +1,8 @@
 # class responsible for taking user input for questions
+from QuizBase import QuizBase
+class QuestionInput(QuizBase):
+    def get_question(self):
+        print("\n⚒️ Let's build a fun quiz!")
 # ask user to input a question, the possible answers, and the correct answer
 # validate the correct answer
 # return all question data as a dictionary
