fab68839f625b66df4273999f54e4784bcbc7624
Linuun <jlenonninolla31@gmail.com>
2025-05-20 23:56:30 +0800

    fixed a typo error

--- FileSaver.py
+++ FileSaver.py
@@ -3,7 +3,7 @@ from QuizBase import QuizBase
 class FileSaver(QuizBase):
     def save_to_file(self, questions_data):
 # open the file in append mode and write the questions and answers
-        with open(self.filename, "a") as file:
+        with open(self.file_name, "a") as file:
             file.write(f"Question: {questions_data['question']}\n")
             file.write(f"A.) {questions_data['A']}\n")
             file.write(f"B.) {questions_data['B']}\n")
